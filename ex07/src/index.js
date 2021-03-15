4650d1ea0510505b7a66fa7ca314fffaac1fc283
enida <enida_mujezin_16@yahoo.com>
2021-03-16 00:37:18 +0100

    add

---
+++ ex07/src/index.js
@@ -0,0 +1,18 @@
+var sum = 0;
+function addThree() {
+    sum = sum + 3;
+}
+var returnedValue = addThree(3);
+var sum = 0;
+function addFive() {
+    sum = sum + 5;
+}
+var returnedValue = addFive(8);
+
+module.exports = {
+
+    addThree,
+
+    addFive
+
+};
